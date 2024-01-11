fe1eee9fd672e66ee5fc74af678077d79c5f184b
tasyaaa037 <118708476+tasyaaa037@users.noreply.github.com>
2024-01-11 19:54:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,24 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Media Online</title>
+    <style>
+        body {
+            text-align: center;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            border: 2px solid #333; 
+            padding: 20px;
+            border-radius: 10px; 
+        }
+    </style>
 </head>
 <body>
-    <h1>Media Online</h1>
-    <h2>Sosial Media Developer</h2>
-    <p>Belajar dan berbagi agar hidup menjadi lebih baik </p>
+    <div class="container">
+        <h1>Media Online</h1>
+        <h2>Sosial Media Developer</h2>
+        <p>Belajar dan berbagi agar hidup menjadi lebih baik </p>
 
-    <h2>Benefit Join di Media Online</h2>
-    <ul>
-    <li>Mendapatkan motivasi dari sesama para developer</li>
-    <li>Sharing knowledge</li>
-    <li>Dibuat oleh calon web developer terbaik</li>
-</ul>
+        <h2>Benefit Join di Media Online</h2>
+        <p>Mendapatkan motivasi dari sesama para developer</p>
+        <p>Dibuat oleh calon web developer terbaik</p>
 
-<h2>Cara Bergabung ke Media Online</h2>
-    <ol>
-        <li>Mengunjungi website ini</li>
-        <li>Mendaftarkan di <a href="index2.php">Form Sign Up</a></li>
-        <li>Selesai</li>
-        </ol>
+        <h2>Cara Bergabung ke Media Online</h2>
+        
+        <p>Mengunjungi website ini</p>
+        <p>Mendaftarkan di <a href="index2.php">Form Sign Up</a></p>
+        <p>Selesai</p>
+    </div>
 </body>
 </html>
